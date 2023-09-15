841ac7cbe4ec17f8d0a69c1c63ad6384ef16fcf6
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-14 20:15:56 -0400

    audio plan set up

---
+++ Old_Harbour Start/Assets/Notes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    /*2D music audio or music to trigger a specific event
+     layer big ocean and small water
+     more intense wind with less intense wind
+     maybe ambient background sound
+     players footsteps
+     reverb on elemental sounds
+        */
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
