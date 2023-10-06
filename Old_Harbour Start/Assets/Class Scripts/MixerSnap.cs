ca59b4b112b59998c2713e08e7f4030cc1862444
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-10-05 21:47:15 -0400

    more

--- Old_Harbour Start/Assets/Class Scripts/MixerSnap.cs
+++ Old_Harbour Start/Assets/Class Scripts/MixerSnap.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -13,13 +14,16 @@ public class MixerSnap : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         indoorSnap.TransitionTo(ChangeSecs);
+        Debug.Log("Trigger Enter" + indoorSnap);
     }
 
 
     private void OnTriggerExit(Collider other)
     {
         outdoorSnap.TransitionTo(ChangeSecs);
+        Debug.Log("Trigger Enter" + outdoorSnap);
     }
+
 }
 
 
